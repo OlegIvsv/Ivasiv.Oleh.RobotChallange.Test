1ef9ad3500339c07faba8f47dc0b87349a0d1d60
Oleh <oleg40512@gmail.com~>
2022-09-03 21:26:00 +0300

    new tests

--- HelpersTest/EnergyHelperTests.cs
+++ HelpersTest/EnergyHelperTests.cs
@@ -13,8 +13,8 @@ namespace Ivasiv.Oleh.RobotChallange.Test.HelpersTest
         [TestMethod]
         public void GnergyToGetTo()
         {
-            Position p1 = new Position(3, 4), p2 = new Position(5, 7);
-            int expected = 13;
+            Position p1 = new Position(3, 4), p2 = new Position(98, 99);
+            int expected = 50;
 
             int result = EnergyHelper.EnergyToGetTo(p1, p2);
 
@@ -38,5 +38,101 @@ namespace Ivasiv.Oleh.RobotChallange.Test.HelpersTest
             Assert.AreEqual(expected, result);
         }
 
+        [DataRow(5, 5, 1)]
+        [DataRow(40, 40, 1)]
+        [DataRow(50, 10, 2)]
+        [DataRow(99, 45, 1)]
+        [TestMethod]
+        public void MinEnergyFromHereToStation(int x, int y, int expectedStationIndex)
+        {
+            var map = new Map()
+            {
+                Stations = new List<EnergyStation>
+                {
+                    new EnergyStation(){ Position = new Position(10, 10)},
+                    new EnergyStation(){ Position = new Position(10, 45)},
+                    new EnergyStation(){ Position = new Position(45, 10)},
+                }
+            };
+            var myRobot = new Robot.Common.Robot() { Position = new Position(x, y), OwnerName = "owner1" };
+            var robots = new List<Robot.Common.Robot>
+            {
+                new Robot.Common.Robot(){ Position = new Position(10, 10), OwnerName = "owner1"},
+                new Robot.Common.Robot(){ Position = new Position(45, 10), OwnerName = "owner2"},
+                myRobot
+            };
+            int expected = EnergyHelper.CanGetToWith(map.Stations[expectedStationIndex].Position, robots, myRobot);
+
+
+            int result = EnergyHelper.MinEnergyFromHereToStation(map, myRobot, robots);
+
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [DataRow(5, 5, 2)]
+        [DataRow(40, 40, 2)]
+        [DataRow(50, 10, 1)]
+        [DataRow(99, 45, 2)]
+        [TestMethod]
+        public void ThenMinEnergyFromHereToStation(int x, int y, int expectedStationIndex)
+        {
+            var map = new Map()
+            {
+                Stations = new List<EnergyStation>
+                {
+                    new EnergyStation(){ Position = new Position(10, 10)},
+                    new EnergyStation(){ Position = new Position(10, 45)},
+                    new EnergyStation(){ Position = new Position(45, 10)},
+                }
+            };
+            var myRobot = new Robot.Common.Robot() { Position = new Position(x, y), OwnerName = "owner1" };
+            var robots = new List<Robot.Common.Robot>
+            {
+                new Robot.Common.Robot(){ Position = new Position(10, 10), OwnerName = "owner1"},
+                new Robot.Common.Robot(){ Position = new Position(45, 10), OwnerName = "owner2"},
+                myRobot
+            };
+            int expected = EnergyHelper.CanGetToWith(map.Stations[expectedStationIndex].Position, robots, myRobot);
+
+
+            int result = EnergyHelper.ThenMinEnergyFromHereToStation(map, myRobot, robots);
+
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [DataRow(1, 40, 1)]
+        [DataRow(10, 90, 2)]
+        [DataRow(10, 9, 1)]
+        [TestMethod]
+        public void MostBeneficialStation(int x, int y, int expectedStationIndex)
+        {
+            var map = new Map()
+            {
+                Stations = new List<EnergyStation>
+                {
+                    new EnergyStation(){ Position = new Position(10, 10)},
+                    new EnergyStation(){ Position = new Position(10, 40)},
+                    new EnergyStation(){ Position = new Position(10, 70)},
+                }
+            };
+            var myRobot = new Robot.Common.Robot() { Position = new Position(x, y), OwnerName = "owner1" };
+            var robots = new List<Robot.Common.Robot>
+            {
+                new Robot.Common.Robot(){ Position = new Position(10, 10), OwnerName = "owner1"},
+                new Robot.Common.Robot(){ Position = new Position(10, 70), OwnerName = "owner2"},
+                myRobot
+            };
+            EnergyStation expected = map.Stations[expectedStationIndex];
+
+
+            EnergyStation result = EnergyHelper.MostBenneficialStation(map, myRobot, robots);
+
+
+            Assert.AreSame(expected, result);
+        }
     }
 }
